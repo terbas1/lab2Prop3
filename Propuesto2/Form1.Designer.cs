38dc298f0ce379fe18bdef8fce1778344af3fef1
Edwin Daniel Baltazar Terbullino <edwin.baltazar@tecsup.edu.pe>
2020-03-09 21:41:04 -0500

    Subiendo

---
+++ Propuesto2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Propuesto2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.boxAnio = new System.Windows.Forms.ComboBox();
+            this.boxMes = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataPedido = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataPedido)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PROPUESTO NUMERO 2";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LISTA DE PEDIDOS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "AÑO:";
+            // 
+            // boxAnio
+            // 
+            this.boxAnio.FormattingEnabled = true;
+            this.boxAnio.Location = new System.Drawing.Point(409, 74);
+            this.boxAnio.Name = "boxAnio";
+            this.boxAnio.Size = new System.Drawing.Size(121, 24);
+            this.boxAnio.TabIndex = 3;
+            // 
+            // boxMes
+            // 
+            this.boxMes.FormattingEnabled = true;
+            this.boxMes.Location = new System.Drawing.Point(650, 73);
+            this.boxMes.Name = "boxMes";
+            this.boxMes.Size = new System.Drawing.Size(121, 24);
+            this.boxMes.TabIndex = 4;
+            this.boxMes.SelectionChangeCommitted += new System.EventHandler(this.boxMes_SelectionChangeCommitted);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(588, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "MES";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dataPedido
+            // 
+            this.dataPedido.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataPedido.Location = new System.Drawing.Point(93, 128);
+            this.dataPedido.Name = "dataPedido";
+            this.dataPedido.RowTemplate.Height = 24;
+            this.dataPedido.Size = new System.Drawing.Size(678, 226);
+            this.dataPedido.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataPedido);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.boxMes);
+            this.Controls.Add(this.boxAnio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataPedido)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox boxAnio;
+        private System.Windows.Forms.ComboBox boxMes;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataPedido;
+    }
+}
+
---
+++ Propuesto2/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Propuesto2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Baltazar"].ConnectionString);
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ListaAnios()
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_ListaAnios", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "ListaAnios");
+                        boxAnio.DataSource = df.Tables["ListaAnios"];
+                        boxAnio.DisplayMember = "Anios";
+                        boxAnio.ValueMember = "Anios";
+
+                    }
+                }
+            }
+        }
+        public void ListaMes()
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_ListaMes", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "ListaMes");
+                        boxMes.DataSource = df.Tables["ListaMes"];
+                        boxMes.DisplayMember = "Mes";
+                        boxMes.ValueMember = "Mes";
+
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaAnios();
+            ListaMes();
+        }
+
+        private void boxMes_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("usp_Pedido_Propuesto", cn))
+            {
+                using (SqlDataAdapter Da = new SqlDataAdapter())
+                {
+                    Da.SelectCommand = cmd;
+                    Da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    Da.SelectCommand.Parameters.AddWithValue("@fechaanio", boxAnio.SelectedValue);
+                    Da.SelectCommand.Parameters.AddWithValue("@fechames", boxMes.SelectedValue);
+                    using (DataSet df = new DataSet())
+                    {
+                        Da.Fill(df, "Pedidos");
+                        dataPedido.DataSource = df.Tables["Pedidos"];
+                        
+                    }
+                }
+            }
+        }
+    }
+}
